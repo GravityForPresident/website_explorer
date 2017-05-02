4cef6f04be3f80be90074735ad64fe7a0ebbe937
GravityForPresident <koppany.botond@gmail.com>
2017-05-02 23:58:15 +0200

    Böngésző váltás
    A két böngésző közötti váltás implementálása.

--- webpageExplorer.py
+++ webpageExplorer.py
@@ -2,11 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 with open('inputFile.txt', 'r') as inputfile:
+	tiptext  = inputfile.readline().replace('\n', '')
 	url = inputfile.readline().replace('\n', '')
 	depth = inputfile.readline().replace('\n', '')
 	browser = inputfile.readline().replace('\n', '')
 
-driver = webdriver.Chrome()
+if browser == "firefox" or browser == "Firefox":
+	driver = webdriver.Firefox()
+elif browser == "chrome" or browser == "Chrome":
+	driver = webdriver.Chrome()
+	
 driver.get(url)
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
