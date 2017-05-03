20f4a42043e816b2d8716493c2e67ba250e01496
GravityForPresident <koppany.botond@gmail.com>
2017-05-03 06:20:15 +0200

    Gyorsított verzió
    Valahol elakad a futás.

--- webpageExplorer.py
+++ webpageExplorer.py
@@ -2,23 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 def getLinkList (url, szint):
-	if browser == "firefox" or browser == "Firefox":
-		driver = webdriver.Firefox()
-	elif browser == "chrome" or browser == "Chrome":
-		driver = webdriver.Chrome()
-	elif browser == "ie" or browser == "IE" or browser == "Ie" or browser == "InternetExplorer" or browser == "internetexplorer" or browser == "Internetexplorer":
-		driver = webdriver.Ie()
-
 	driver.get(url)
-	#assert "Python" in driver.title
 	elems = driver.find_elements_by_tag_name('a')
 	tempfile = open('tempFile.txt', "a")
 	for elem in elems:
-			my_string = str(elem.get_attribute("href"))
-			if my_string.find("html") > -1:
-				tempfile.write(szint + my_string + "\n")
+		my_string = str(elem.get_attribute("href"))
+		if my_string.find("http") > -1:
+			tempfile.write(szint + my_string + "\n")
 	tempfile.close()
-	driver.close()
 	
 with open('inputFile.txt', 'r') as inputfile:
 	tiptext  = inputfile.readline().replace('\n', '')
@@ -26,6 +17,13 @@ with open('inputFile.txt', 'r') as inputfile:
 	depth = inputfile.readline().replace('\n', '')
 	browser = inputfile.readline().replace('\n', '')
 inputfile.close()
+
+if browser == "firefox" or browser == "Firefox":
+	driver = webdriver.Firefox()
+elif browser == "chrome" or browser == "Chrome":
+	driver = webdriver.Chrome()
+elif browser == "ie" or browser == "IE" or browser == "Ie" or browser == "InternetExplorer" or browser == "internetexplorer" or browser == "Internetexplorer":
+	driver = webdriver.Ie()
 
 outputfile = open('outputFile.txt', "w")
 outputfile.write(url + "\n")
@@ -36,13 +34,16 @@ temp.close()
 
 depth = int(depth)
 level = ""
+cutLine = ""
 
 for i in range(0, depth):
 	levelPlusOne = level + "\t"
 	inputfile2  = open('outputFile.txt', "r")
 	for line in inputfile2:
 		if line.find(level) > -1:
-			getLinkList(line, levelPlusOne)
+			start = len(level)
+			cutLine = line[start:]
+			getLinkList(cutLine, levelPlusOne)
 		else:
 			temp  = open('tempFile.txt', "a")
 			temp.write(line)
@@ -59,3 +60,6 @@ for i in range(0, depth):
 	temp  = open('tempFile.txt', "w")
 	temp.write("")
 	temp.close()
+	#level = "\t" + level
+
+driver.close()
